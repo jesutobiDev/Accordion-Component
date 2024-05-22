74b1c4f91c04f70af1c80d0a7159fa653e73c6f4
jesutobiDev <jesutobi_dev@outlook.com>
2024-05-22 22:54:41 +0100

    transition

--- src/script.js
+++ src/script.js
@@ -1,27 +1,26 @@
 function toggleAnswer(id) {
-    const answerElement = document.getElementById(id);
-    const isOpen = answerElement.getAttribute('answer-is-open') === 'true';
-  
-    // Toggle the answer visibility
-    answerElement.setAttribute('answer-is-open', !isOpen);
-    if (isOpen) {
-      answerElement.classList.add('hidden');
-    } else {
-      answerElement.classList.remove('hidden');
-    }
-  
-    // Get the icons associated with this answer
-    const parentElement = answerElement.parentElement.previousElementSibling;
-    const plusIcon = parentElement.querySelector('.plus-icon');
-    const minusIcon = parentElement.querySelector('.minus-icon');
-  
-    // Toggle the icons
-    if (isOpen) {
-      plusIcon.classList.remove('hidden');
-      minusIcon.classList.add('hidden');
-    } else {
-      plusIcon.classList.add('hidden');
-      minusIcon.classList.remove('hidden');
-    }
+  const answerElement = document.getElementById(id);
+  const isOpen = answerElement.getAttribute('answer-is-open') === 'true';
+
+  // Toggle the answer visibility
+  answerElement.setAttribute('answer-is-open', !isOpen);
+  if (isOpen) {
+    answerElement.classList.remove('max-h-[300px]', 'pt-3');
+  } else {
+    answerElement.classList.add('max-h-[300px]', 'pt-3');
   }
-  
+
+  // Get the icons associated with this answer
+  const parentElement = answerElement.parentElement;
+  const plusIcon = parentElement.querySelector('.plus-icon');
+  const minusIcon = parentElement.querySelector('.minus-icon');
+
+  // Toggle the icons
+  if (isOpen) {
+    plusIcon.classList.remove('hidden');
+    minusIcon.classList.add('hidden');
+  } else {
+    plusIcon.classList.add('hidden');
+    minusIcon.classList.remove('hidden');
+  }
+}
